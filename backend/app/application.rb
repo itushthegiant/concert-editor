c20f1bb487323342636064800f7150886ef11ede
etamar <etamar.levy@gmail.com>
2021-08-26 16:53:10 -0400

    3 http requests are in the controller

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -18,17 +18,23 @@ class Application
     elsif @req.path.match(/artists/) && @req.get?
         artists = Artist.all
         return [200, { 'Content-Type' => 'application/json' }, [ artists.to_json ]]
-    elsif @req.path.match(/concerts/)
-
+    elsif @req.path.match(/artists/) && @req.post?
+        data = JSON.parse @req.body.read
+        artist = Artist.create(data)
+        return [200, { 'Content-Type' => 'application/json' }, [ artist.to_json ]]
+    elsif @req.delete?
+      id = @req.path_info.split('/artists/').last
+      artist = Artist.find(id)
+      artist.delete
+      return [200, { 'Content-Type' => 'application/json' }, [ {message: 'Artist deleted'}.to_json ]]
     else
       resp.write "Path Not Found"
     end
-
     resp.finish
   end
 
 
-
+# checks if there is a number
   def params_id
     @req.path_info[/\d+/]
   end
