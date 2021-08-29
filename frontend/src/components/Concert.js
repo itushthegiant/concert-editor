74c7a227c842505b41ab0d15a54156e5c0ad1b51
etamar <etamar.levy@gmail.com>
2021-08-29 14:45:54 -0400

    concerts cards layout fixed

--- frontend/src/components/Concert.js
+++ frontend/src/components/Concert.js
@@ -6,16 +6,18 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
 
 
 const useStyles = makeStyles({
     root: {
-      maxWidth: 345,
+        maxWidth: 345,
     },
     media: {
-      height: 140,
+        height: 140,
     },
-  });
+});
 
 
 export default function Concert(props) {
@@ -24,35 +26,38 @@ export default function Concert(props) {
 
     return (
         <div>
-            <Card className={classes.root}>
-                <CardActionArea>
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            {props.conData.title}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            Genre: {props.conData.artist.genre}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            Date: {props.conData.date}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            Venue: {props.conData.venue.name}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            Address: {props.conData.venue.location}
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                        Share
-                    </Button>
-                    <Button size="small" color="primary">
-                        Learn More
-                    </Button>
-                </CardActions>
-            </Card>
+            <Box component="span" m={3} className="card-box">
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                {props.conData.title}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                Genre: {props.conData.artist.genre}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                Date: {props.conData.date}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                Venue: {props.conData.venue.name}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                Address: {props.conData.venue.location}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" color="primary">
+                            Share
+                        </Button>
+                        <Button size="small" color="primary">
+                            Learn More
+                        </Button>
+                    </CardActions>
+                </Card>
+            </Box>
+
         </div>
     )
 }
--- frontend/src/containers/Concerts.js
+++ frontend/src/containers/Concerts.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Concert from '../components/Concert'
+import Grid from '@material-ui/core/Grid';
+
 
 
 
@@ -31,7 +33,15 @@ export default class Concerts extends Component {
     render() {
         return (
             <div>
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+
                 {this.concertsToShow()}
+                </Grid>
             </div>
         )
     }
