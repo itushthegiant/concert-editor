0206d824bd534ce399d0c533a16e2dbdff8f1857
etamar <etamar.levy@gmail.com>
2021-08-31 19:09:44 -0400

    db droped and reseeded

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,11 +1,13 @@
 require 'faker'
 require 'pry'
 
-# 10.times do
+# 70.times do
 #     Artist.create(name: Faker::Music.band, genre: Faker::Music.genre)
 # end
 
-# 15.times do
+# 30.times do
 #     Venue.create(name: Faker::FunnyName.name_with_initial + " Hall", location: Faker::Address.full_address)
 # end
 
+# Concert.create(title: "Program The Dead in Pepe C. Cola Hall", venue_id: 17, artist_id: 41, date: Faker::Date.forward)
+# Concert.create(title: "Catherine Wheel in Phil A. Mignon Hall", venue_id: 18, artist_id: 42, date: Faker::Date.forward)
